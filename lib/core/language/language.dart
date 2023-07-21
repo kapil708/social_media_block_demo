87e5ed92aa73a1c2812777f57379e2b39092bcee
Kapil Ramkhiladi Singh <singhkapil708@gmail.com>
2023-07-21 15:03:06 +0530

    - localization quick fix

--- lib/core/language/language.dart
+++ lib/core/language/language.dart
@@ -3,16 +3,8 @@ import 'dart:ui';
 import '../../core/assets/image_assets.dart';
 
 enum Language {
-  english(
-    Locale('en', 'US'),
-    ImageAssets.english,
-    'English',
-  ),
-  indonesia(
-    Locale('id', 'ID'),
-    ImageAssets.indonesia,
-    'Bahasa Indonesia',
-  );
+  english(Locale('en', 'US'), ImageAssets.english, 'English'),
+  indonesia(Locale('id', 'ID'), ImageAssets.indonesia, 'Bahasa Indonesia');
 
   const Language(this.value, this.image, this.text);
 
